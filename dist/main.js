1ef12bacb2ac6eeddcb6f9501e091c92f7a03e68
Sergey Korolev <bongrichard@rambler.ru>
2025-02-06 18:28:07 +0300

    в файл functions/clock.js - добавлен блок с часами, ему присвоены стилли + функия расчета времени и записи ее в блок счасами которая отрабатывает один раз в секунду

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,21 @@
-(()=>{"use strict";console.log("Привет")})();
+(() => {
+  "use strict";
+  !(function () {
+    const e = document.createElement("div");
+    function n() {
+      const n = new Date(),
+        t = new Intl.DateTimeFormat("ru-Ru", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        }).format(n);
+      e.innerText = t;
+    }
+    (e.draggable = !0),
+      e.classList.add("clock_style_one"),
+      setInterval(n, 1e3),
+      n(),
+      document.body.append(e),
+      console.log("Привет");
+  })();
+})();
--- functions/clock.js
+++ functions/clock.js
@@ -1,3 +1,22 @@
 export function statr_app_clock() {
+  const clock = document.createElement("div"); // создание блока с часами
+  clock.draggable = true; // разрешаем перемещение блока счасами
+
+  clock.classList.add("clock_style_one"); // добавляем стили блоку с часами
+
+  setInterval(updateClock, 1000); // вызываем функцию updateClock по интерваллу 1-а сек.
+  updateClock(); // первичный вызов функцию updateClock
+  document.body.append(clock); // добавляем блок с часами в тело страницы
+
+  function updateClock() {
+    // фунция полученя времени и вывода его в блок с часами
+    const date = new Date();
+    const time = new Intl.DateTimeFormat("ru-Ru", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(date);
+    clock.innerText = time;
+  }
   console.log("Привет");
 }
