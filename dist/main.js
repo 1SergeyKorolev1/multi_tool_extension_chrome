8f53846c8c064a8a44edba6f07c90bc9ea209fb8
Sergey Korolev <bongrichard@rambler.ru>
2025-02-06 18:51:09 +0300

    добавллен сллушатель на сочетание клавишь ctrl+M - убираем/добавлляем клас видимости + записываем информацию о изменениях в хранилище чтоб при обновлении страниц данные не терялись

--- dist/main.js
+++ dist/main.js
@@ -4,18 +4,30 @@
     const e = document.createElement("div");
     function n() {
       const n = new Date(),
-        t = new Intl.DateTimeFormat("ru-Ru", {
+        o = new Intl.DateTimeFormat("ru-Ru", {
           hour: "numeric",
           minute: "numeric",
           second: "numeric",
         }).format(n);
-      e.innerText = t;
+      e.innerText = o;
     }
     (e.draggable = !0),
-      e.classList.add("clock_style_one"),
+      e.classList.add("clock_style_one", "display_none_all"),
       setInterval(n, 1e3),
       n(),
       document.body.append(e),
+      chrome.storage.local.get(["display"], (n) => {
+        n.display && e.classList.remove("display_none_all");
+      }),
+      chrome.storage.onChanged.addListener(function (n, o) {
+        n.display && e.classList.toggle("display_none_all");
+      }),
+      document.addEventListener("keydown", function (n) {
+        "KeyM" === n.code &&
+          chrome.storage.local.set({
+            display: e.classList.contains("display_none_all"),
+          });
+      }),
       console.log("Привет");
   })();
 })();
--- functions/clock.js
+++ functions/clock.js
@@ -2,11 +2,33 @@ export function statr_app_clock() {
   const clock = document.createElement("div"); // создание блока с часами
   clock.draggable = true; // разрешаем перемещение блока счасами
 
-  clock.classList.add("clock_style_one"); // добавляем стили блоку с часами
+  clock.classList.add("clock_style_one", "display_none_all"); // добавляем стили блоку с часами
 
   setInterval(updateClock, 1000); // вызываем функцию updateClock по интерваллу 1-а сек.
   updateClock(); // первичный вызов функцию updateClock
   document.body.append(clock); // добавляем блок с часами в тело страницы
+
+  // пороверяем соостояние объекта display и прячем блок с часамим в случае true
+  chrome.storage.local.get(["display"], (result) => {
+    if (result.display) clock.classList.remove("display_none_all");
+  });
+
+  // следим за изменениями объекта display - убираем/добавляем видимость блоку с часамим
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if (changes.display) {
+      clock.classList.toggle("display_none_all");
+    }
+  });
+
+  // отслеживаем нажатие комб. ctrl+M и меняем объект display
+  document.addEventListener("keydown", function (event) {
+    if (event.code === "KeyM") {
+      // console.log("%cРаботает", "font-size:24px;color:blue;");
+      chrome.storage.local.set({
+        display: clock.classList.contains("display_none_all"),
+      });
+    }
+  });
 
   function updateClock() {
     // фунция полученя времени и вывода его в блок с часами
